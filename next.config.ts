accfad159e94251197e7f0dadbad8186cc84e022
akshattyagi03 <tyagiakshat123@gmail.com>
2026-05-05 19:54:38 +0530

    config file

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  output: "standalone",
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "financialmodelingprep.com",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      }
     ],
   },
 };
